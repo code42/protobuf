4b9dd42dbad095f18b537994865ea0a87023f104
John Dullum <john.dullum@code42.com>
2016-10-31 14:04:48 -0500

    Bumped the version to 2.7.0

--- python/setup.py
+++ python/setup.py
@@ -156,7 +156,7 @@ if __name__ == '__main__':
         ))
 
   setup(name = 'protobuf',
-        version = '2.6.1',
+        version = '2.7.1',
         packages = [ 'google' ],
         namespace_packages = [ 'google' ],
         test_suite = 'setup.MakeTestSuite',
